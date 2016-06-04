5081bbaf13a35f7142c6fd50996889b277586c38
Michael West <mwest@mdsol.com>
2016-06-04 12:35:08 -0400

    Reduce and clean up

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,10 @@
-var shouldDisplayMarkers = true;
-
-var graph = generateGraph();
-graph.colorGraph(4);
-
 var map = L.map('map').setView([40.7127, -74.0059], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mpwest929.cdc89604',
-    accessToken: 'access-token'
+    accessToken: 'pk.eyJ1IjoibXB3ZXN0OTI5IiwiYSI6IjkzYzUxMTY4NTQ5NzEzOWY3NWU1MWY3YzNiMTUwMGIxIn0.j5Pp4ifPG1I4ZIMaGtKtqA'
 }).addTo(map);
-
-/*var recycleMarker = L.AwesomeMarkers.icon({
-  icon: 'recycle',
-  prefix: 'fa',
-  markerColor: 'green'
-});*/
 
 var neighborhoods = [];
 $.get('/neighborhoods', function(data, status) {
@@ -45,34 +34,9 @@ $.get('/neighborhoods', function(data, status) {
     },
     style: function(feature) {
       switch(feature.properties.borough) {
-        case 'Bronx':
-          var node = graph.findNode(feature.properties.neighborhood);
-
-          if (node !== undefined) {
-            return getColorized(node.attrs.color);
-          } else {
-            console.log(`Couldn't find a node for ${feature.properties.neighborhood}. Using default styling.`);
-            return bronxStyle;
-          }
-        case 'Brooklyn':
-          var node = graph.findNode(feature.properties.neighborhood);
-
-          if (node !== undefined) {
-            return getColorized(node.attrs.color);
-          } else {
-            console.log(`Couldn't find a node for ${feature.properties.neighborhood}. Using default styling.`);
-            return brooklynStyle;
-          }
-        case 'Manhattan':
-          var node = graph.findNode(feature.properties.neighborhood);
-
-          if (node !== undefined) {
-            return getColorized(node.attrs.color);
-          }
-          else {
-            console.log(`Couldn't find a node for ${feature.properties.neighborhood}. Using default styling.`);
-            return manhattanStyle;
-          }
+        case 'Bronx': return bronxStyle;
+        case 'Brooklyn': return brooklynStyle;
+        case 'Manhattan': return manhattanStyle;
         case 'Queens': return queensStyle;
         case 'Staten Island': return statenIslandStyle;
       }
@@ -81,13 +45,6 @@ $.get('/neighborhoods', function(data, status) {
 
   var source = null;
   neighborhoodLayer.on("click", function (e) {
-    if (source === null) {
-      source = e.layer.feature.properties.neighborhood;
-    }
-    else {
-      var destination = e.layer.feature.properties.neighborhood;
-      console.log(`graph.addEdge('${source}', '${destination}');`);
-      source = null;
-    }
+    //TODO: Implement
   });
 });
