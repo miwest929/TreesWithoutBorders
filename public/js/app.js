e489a2df0f66183886bc00457b436d9bf74d1f22
Michael West <mwest@mdsol.com>
2016-06-04 13:49:16 -0400

    Disable scrollwheel zooming. Add year buttons

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,10 @@
 $("#map").height($(window).height()).width($(window).width());
 
 var map = L.map('map').setView([40.7127, -74.0059], 11);
+map.scrollWheelZoom.disable();
+map.dragging.disable();
+map.touchZoom.disable();
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
